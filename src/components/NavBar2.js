e094634345b90ea0d0a8445d76fb09c309e0c20f
Sam Phan <sphan@pros.com>
2021-11-12 11:39:42 -0600

    Updating the NavBar2 to not have a logout option. Also added an example auth_config file

--- src/components/NavBar2.js
+++ src/components/NavBar2.js
@@ -10,23 +10,11 @@ import {
   Nav,
   NavItem,
   NavLink,
-  Button
 } from "reactstrap";
-
-import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const {
-    isAuthenticated,
-    logout,
-  } = useAuth0();
   const toggle = () => setIsOpen(!isOpen);
-
-  const logoutWithRedirect = () =>
-    logout({
-      returnTo: window.location.origin,
-    });
 
   return (
     <div className="nav-container">
@@ -46,20 +34,6 @@ const NavBar = () => {
                 </NavLink>
               </NavItem>
             </Nav>
-            {!isAuthenticated && (
-            <Nav className="d-none d-md-block" navbar>
-                <NavItem>
-                   <Button
-                        id="qsLoginBtn"
-                        color="primary"
-                        className="btn-margin"
-                        onClick={() => logoutWithRedirect()}
-                    >
-                Log out
-        </Button>
-             </NavItem>  
-             </Nav> 
-              )}
           </Collapse>
         </Container>
       </Navbar>
